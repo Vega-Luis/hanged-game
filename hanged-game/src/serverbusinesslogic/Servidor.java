b7d821d6c4255df47355294da10272d0ce52da8a
Marcos Méndez Hidalgo <marcosmh0199@gmail.com>
2019-04-27 23:47:47 -0600

    feat: server performance
    The server can create a game session & manage it.

--- hanged-game/src/serverbusinesslogic/Servidor.java
+++ hanged-game/src/serverbusinesslogic/Servidor.java
@@ -14,50 +14,88 @@ public class Servidor {
   
   public Servidor(int puerto, int tamanoCola) throws IOException {
      serverSocket = new ServerSocket(puerto, tamanoCola);
-     game = new HangedGame();
   }
   
-  private void ejecutarComando(String comando) {
+  private boolean ejecutarComando(String comando) {
     if(comando.equals("initGame")) {
+      game = new HangedGame();
       game.startGame();
     }else if(comando.equals("checkletter")) {
-      
+      if(game.equals(null)) {
+        System.out.println("No hay una partida iniciada\n");
+        return false;
+      }else {
+        game.checkLetter(ingresarLetra(), game.getCurrentWord());
+        if(game.getHits() == game.getCurrentWord().length()) {
+          game = null;
+          return false;
+        }
+        if(game.getFails() == 5) {
+          game.gameOver();
+          game = null;
+          return false;
+        }
+      }
     }else {
       System.out.println("Comando inválido");
+      return true;
     }
+    return true;
   }
 
   public void run() {
      Socket socket; 
      while(true) {
-        try {
-           System.out.println("Esperando cliente en puerto: " + serverSocket.getLocalPort() + "...");
-           // Esperar conexiones
-           socket = serverSocket.accept();
-           
-           System.out.println("Se acaba de conectar: " + socket.getRemoteSocketAddress());
+       try {
+         System.out.println("Esperando cliente en puerto: " + serverSocket.getLocalPort() + "...");
+         // Esperar conexiones
+         socket = serverSocket.accept();
+         System.out.println("Se acaba de conectar: " + socket.getRemoteSocketAddress());
+         boolean over = true;
+         while(over) {
+           System.out.println("Ingrese un comando: ");
            DataInputStream in = new DataInputStream(socket.getInputStream());
-           
-           System.out.println(in.readUTF());
-           DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-           out.writeUTF("Thank you for connecting to " + socket.getLocalSocketAddress()
-              + "\nGoodbye!");
-          
-           socket.close(); 
+           System.out.println();
+           over = ejecutarComando(in.readUTF());
+         }
+     
+         //System.out.println(in.readUTF());
+         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+         out.writeUTF("Thank you for connecting to " + socket.getLocalSocketAddress()
+         + "\nGoodbye!");
+         socket.close(); 
         } 
-        
         catch (SocketTimeoutException s) {
-           System.out.println("Socket timed out!");
-           break;
+          System.out.println("Socket timed out!");
+          break;
         } 
-        
-        catch (IOException e) {
-           e.printStackTrace();
-           break;
+       catch (IOException e) {
+         e.printStackTrace();
+         break;
         }
      }
   }
   
+  private char ingresarLetra() {
+    Socket socket = null;
+    try {
+      socket = serverSocket.accept();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    DataInputStream in = null;
+    try {
+      in = new DataInputStream(socket.getInputStream());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      return in.readChar();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return 0; 
+  }
   public static void main(String[] args) {
      int puerto = Integer.parseInt(args[0]);
      int cola = Integer.parseInt(args[1]);
